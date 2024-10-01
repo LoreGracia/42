b7904568c19e629ac90f3a5b06b8eacf590b917f
Lorena Gracia Fargas <lgracia-@cbr7s5.42barcelona.com>
2024-10-01 20:09:05 +0200

    a

--- Push_swap/ft_sort_small_stack.c
+++ Push_swap/ft_sort_small_stack.c
@@ -6,40 +6,62 @@
 /*   By: lgracia- <lgracia-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 10:47:23 by lgracia-          #+#    #+#             */
-/*   Updated: 2024/09/30 12:15:34 by lgracia-         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:35:29 by lgracia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	if_simple_compare(t_stack **a, t_stack *tmp)
+{
+	if ((*a)->num > (*a)->next->num)
+		ft_swap(a, 'a');
+	else if ((*a)->num > tmp->num)
+		ft_rotate(a, 'a');
+}
+
 /*void	ft_sort_small_stack(t_stack **a, t_stack **b)
 {
 	t_stack	*tmp;
 
-
 	while (ft_ordered(a, b) != 1)
 	{
-		tmp = *a;
-		while ((*a)->num >= tmp->num)
-		{
-			if (!tmp->next)
-			{
-				ft_rotate(a, 'a');
-				break ;
-			}
-			tmp = tmp->next;
-		}
+		if (first_is_biggest(a) == 1)
+		ft_rotate(a, 'a');
 		tmp = *a;
 		while (tmp->next)
 			tmp = tmp->next;
-		if ((*a)->num > (*a)->next->num)
-			ft_swap(a, 'a');
-		else if ((*a)->num > tmp->num)
-			ft_rotate(a, 'a');
-		else
-			ft_reverse(a, 'a');
+		if_simple_compare(a, tmp);
 	}
 }*/
+
+static int	last_is_smallest(t_stack **a, t_stack *tmp)
+{
+	t_stack *stack;
+
+	stack = *a;
+	while (stack->num <= tmp->num)
+		{
+			if (!stack->next)
+				return (1);
+			stack = stack->next;
+		}
+	return (0);
+}
+
+static int	first_is_biggest(t_stack **a)
+{
+	t_stack *tmp;
+
+	tmp = *a;
+	while ((*a)->num >= tmp->num)
+		{
+			if (!tmp->next)
+				return (1);
+			tmp = tmp->next;
+		}
+	return (0);
+}
 
 void	ft_sort_small_stack(t_stack **a, t_stack **b)
 {
@@ -48,23 +70,23 @@ void	ft_sort_small_stack(t_stack **a, t_stack **b)
 	while (ft_ordered(a, b) != 1)
 	{
 		tmp = *a;
-		while ((*a)->num >= tmp->num)
-		{
-			if (!tmp->next)
-			{
-				ft_rotate(a, 'a');
-				break ;
-			}
-			tmp = tmp->next;
-		}
-		tmp = *a;
 		while (tmp->next)
 			tmp = tmp->next;
-		if ((*a)->num > (*a)->next->num)
+		  	//ft_reverse(a, 'a');
+		//if_simple_compare(a, tmp);
+		if (first_is_biggest(a) == 1)
+			ft_rotate(a, 'a');
+		else if ((*a)->num > (*a)->next->num)
 			ft_swap(a, 'a');
 		else if ((*a)->num > tmp->num)
 			ft_rotate(a, 'a');
-		else
+		else if(last_is_smallest(a, tmp) == 1 && first_is_biggest(a) == 1)
+		{
+			ft_push(a, b, 'b');
 			ft_reverse(a, 'a');
+			ft_push(b, a, 'a');
+		}
+		else
+			ft_rotate(a, 'a');
 	}
 }
