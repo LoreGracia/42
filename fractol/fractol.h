e52ac3e924e9390bf8f365a15d88cf59d246168f
Lorena Gracia Fargas <lgracia-@car10s1.42barcelona.com>
2024-10-27 19:33:31 +0100

    Pandad

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: lgracia- <lgracia-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:32:41 by lgracia-          #+#    #+#             */
-/*   Updated: 2024/10/26 17:28:22 by lgracia-         ###   ########.fr       */
+/*   Updated: 2024/10/27 15:57:46 by lgracia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
-# include <unistd.h>
+# include <unistd.h> 
+# include <math.h>
 # include "MLX/include/MLX42/MLX42_Int.h"
 # include "printf/ft_printf.h"
 
--- fractol/main.c
+++ fractol/main.c
@@ -6,13 +6,13 @@
 /*   By: lgracia- <lgracia-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:20:09 by lgracia-          #+#    #+#             */
-/*   Updated: 2024/10/26 18:27:58 by lgracia-         ###   ########.fr       */
+/*   Updated: 2024/10/27 18:38:00 by lgracia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int main(void)
+/*int main(void)
 {
 	void	*mlx;
 	mlx_image_t* img;
@@ -26,4 +26,71 @@ int main(void)
 	mlx_put_pixel(img, 0,0, 0xFF0000FF);
 	mlx_put_pixel(img, 10, 10, 0xFF0000FF);
 	mlx_loop(mlx);
+}*/
+
+static void ft_hook(mlx_key_data_t keydata, void* param)
+{
+	mlx_t *a;
+	a = param;
+	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
+		puts("Panda ");
+
+	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
+		puts("lazyyyyyyyyy");
+
+	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
+		puts("y");
+}
+
+void	cercle(int cx, int cy, int r, mlx_image_t* img, unsigned int color)
+{
+	int	i;
+	int	x;
+	int	y;
+
+	while (r--)
+	{
+		i = 0;
+		while (i++ < 360)
+		{
+			x = (r * sin(i)) + cx;
+			y = (r * cos(i)) + cy;
+			mlx_put_pixel(img, x, y, color);
+		}
+	}
+}
+
+int panda()
+{
+	mlx_t* mlx;
+	mlx_image_t* img;
+	mlx = mlx_init(WIDTH, HEIGHT, "Test", false);
+	if (!mlx)
+        return (0);
+	img = mlx_new_image(mlx, 812, 812);
+	if (!img)
+        return (0);
+	memset(img->pixels, 120, img->width * img->height * sizeof(int32_t));
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        return (0);
+	cercle(WIDTH/2 - 200, WIDTH/2 - 200, 100, img, 0x00000000);
+	cercle(WIDTH/2 + 200, WIDTH/2 - 200, 100, img, 0x00000000);
+	cercle(WIDTH/2, WIDTH/2, 300, img, 0xFFFFFFFF);
+	cercle(WIDTH/2 - 150, WIDTH/2, 50, img, 0x00000000);
+	cercle(WIDTH/2 + 150, WIDTH/2, 50, img, 0x00000000);
+	cercle(WIDTH/2, WIDTH/2 + 100, 30, img, 0x0000000);
+	mlx_key_hook(mlx, &ft_hook, NULL);
+	mlx_loop(mlx);
+	mlx_delete_image(mlx, img);
+	mlx_terminate(mlx);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (0);
+	if (argv[1][0] == 'p')
+		panda();
+	return (EXIT_SUCCESS);
 }
