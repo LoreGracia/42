c7c6a6dcbfa88797bff572a159a0fbfc263febaf
Lorena Gracia Fargas <lgracia-@car3s6.42barcelona.com>
2024-10-29 19:42:04 +0100

    screen fuction works

--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: lgracia- <lgracia-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 11:20:09 by lgracia-          #+#    #+#             */
-/*   Updated: 2024/10/29 15:30:55 by lgracia-         ###   ########.fr       */
+/*   Updated: 2024/10/29 19:41:07 by lgracia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,19 +90,18 @@ int panda()
 void	screen(mlx_image_t *img, int y, unsigned int color)
 {
 	int x;
+	size_t	i;
 
 	x = 0;
-	while (x <= WIDTH)
+	i = 0;
+	while (x <= (int)img->width && y != (int)img->height)
 	{
-		printf("x %d WIDTH %d\n", x, WIDTH);
 		x++;
 		mlx_put_pixel(img, x, y, color);
-	}
-	printf("y %d\n", y);
-	if (y <= WIDTH)
-	{
-		screen(img++, y++, color);
-		printf("y %d\n", y);
+		if (x == (int)img->width && y < (int)img->height)
+			screen(img, y+1, color);
+		if (x == (int)img->width && y == (int)img->height)
+			break ;
 	}
 }
 
@@ -114,16 +113,16 @@ int	line()
 	mlx = mlx_init(WIDTH, HEIGHT, "Test", false);
 	if (!mlx)
 		return (0);
-	img = mlx_new_image(mlx, 812, 812);
+	img = mlx_new_image(mlx, 400, 300);
 	if (!img)
 		return (0);
 	memset(img->pixels, 120, img->width * img->height * sizeof(int32_t));
-	screen(img, 0, 255);
+	screen(img, 120, 0xFFFFFFFF);
 	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
 		return (0);
 	i = 0;
 	while (i++ < 100)
-		mlx_put_pixel(img, 100 + i, 100, 0xFFFFFFFF);
+		mlx_put_pixel(img, 100 + i, 100, 0xFFFFF);
 	mlx_loop(mlx);
 	mlx_delete_image(mlx, img);
 	mlx_terminate(mlx);
